f7e5dc5f0ac6c0ff19e1217016a112c638c13358
taohua <max.liu@xuantonginfo.com>
2018-11-14 20:10:06 +0800

    测试demo完毕

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,21 +1,43 @@
 import * as React from 'react'
+import './Hello.css'
 
 export interface Props {
   name: string,
   level?: number
 }
 
-function Hello({ name, level = 3 }: Props) {
-  if (level <= 0) {
-    throw new Error('You has no access!')
+interface State {
+  currentLevel: number
+}
+
+class Hello extends React.Component<Props, State> {
+  constructor (props: Props) {
+    super(props)
+    this.state = {currentLevel: props.level || 3}
   }
-  return (
-    <div className="hello">
-      <div className="greeting">
-        Hello {name + getMarks(level)}
+  onIncrement = () => this.updateLevel(this.state.currentLevel + 1)
+  onDecrement = () => this.updateLevel(this.state.currentLevel - 1)
+  updateLevel(currentLevel: number) {
+    this.setState({
+      currentLevel
+    })
+  }
+  render () {
+    const { name } = this.props
+    const { currentLevel: level } = this.state
+    if (level <= 0) {
+      throw new Error('You could be a little more enthusiastic. :D');
+    }
+    return (
+      <div className="hello">
+        <div className="greeting">
+          Hello {name + getMarks(level)}
+        </div>
+        <button onClick={this.onDecrement}>---</button>
+        <button onClick={this.onIncrement}>+++</button>
       </div>
-    </div>
-  )
+    )
+  }
 }
 export default Hello
 
