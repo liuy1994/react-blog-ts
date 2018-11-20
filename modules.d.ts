aea432cbdc411d8b3ab0a96855d7d8f6826e67b3
taohua <max.liu@xuantonginfo.com>
2018-11-20 09:13:57 +0800

    终于解决了问题，开始开发

--- modules.d.ts
+++ modules.d.ts
@@ -6,19 +6,8 @@ declare module 'axios' {
   const axios: any;
   export default axios;
 }
-// declare module 'redux-persist' {
-//   const reduxPersist: any;
-//   export default reduxPersist;
-// }
-// declare module 'redux-redux' {
-//   const reduxRedux: any;
-//   export default reduxRedux;
-// }
-// declare module 'redux' {
-//   const redux: any;
-//   export default redux;
-// }
-declare module 'redux';
-declare module 'redux-persist';
+// declare module 'redux';
+declare module 'react-redux';
+// declare module 'redux-persist';
 declare module 'redux-persist/es/integration/react';
 declare module 'redux-persist/es/storage';
--- src/redux/notelist.ts
+++ src/redux/notelist.ts
@@ -2,10 +2,13 @@ interface action {
   type: string,
   id: number
 }
+interface State {
+  selectedNoteId: any
+}
 const defaultState = {
   selectedNoteId: null
 }
-const notelist = (state = defaultState, action: action) => {
+const notelist = (state = defaultState, action: action): State => {
   switch (action.type) {
     case 'SELECT_NOTE':
       return Object.assign({}, state, {
