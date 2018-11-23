3ef7bf673a88f255bb6fdbd66c9d5e1c9716c564
taohua <max.liu@xuantonginfo.com>
2018-11-23 14:25:50 +0800

    modify

--- src/coms/ContentList.tsx
+++ src/coms/ContentList.tsx
@@ -66,41 +66,32 @@ class ContentList extends Component<Props, State> {
   render() {
     let { contentList, searched } = this.state
     let noteId = this.props.selectedNoteId
-    // if (noteId || searched) {
-      return (
-        <div className="content-list">
-          <Row>
-            {noteId ? <Link className="add-content" to="/add"><Button size="small" type="primary">新增文章</Button></Link> : null}
-            <Search placeholder="input search text" onSearch={value => this.search(value)} enterButton />
-          </Row>
-          {
-            (noteId || searched) ? 
-              <List
-                bordered={true}
-                dataSource={contentList}
-                renderItem={item => (
-                  <List.Item key={item.id}>
-                    <List.Item.Meta
-                      title={<Link to={`detail/${item.id}`}>{item.name}</Link>}
-                      description={item.brief}
-                    />
-                    <Link to={`/edit/${item.id}`}><Button type="primary">编辑</Button></Link>
-                    <Button type="danger" onClick={this.showDeleteItem.bind(this, item.id)}>删除</Button>
-                  </List.Item>
-                )}
-              ></List> : 
-              <h1>Welcome, 选择一个笔记本或者输入关键字搜索</h1>
-          }
-          
-        </div>
-      )
-    // } else {
-    //   return (
-    //     <div>
-    //       <Search placeholder="input search text" onSearch={value => this.search(value)} enterButton />
-    //     </div>  
-    //   )
-    // }
+    return (
+      <div className="content-list">
+        <Row>
+          {noteId ? <Link className="add-content" to="/add"><Button size="small" type="primary">新增文章</Button></Link> : null}
+          <Search placeholder="input search text" onSearch={value => this.search(value)} enterButton />
+        </Row>
+        {
+          (noteId || searched) ? 
+            <List
+              bordered={true}
+              dataSource={contentList}
+              renderItem={item => (
+                <List.Item key={item.id}>
+                  <List.Item.Meta
+                    title={<Link to={`detail/${item.id}`}>{item.name}</Link>}
+                    description={item.brief}
+                  />
+                  <Link to={`/edit/${item.id}`}><Button type="primary">编辑</Button></Link>
+                  <Button type="danger" onClick={this.showDeleteItem.bind(this, item.id)}>删除</Button>
+                </List.Item>
+              )}
+            ></List> : 
+            <h1>Welcome, 选择一个笔记本或者输入关键字搜索</h1>
+        }
+      </div>
+    )
   }
 }
 const mapStateToProps = ({ notelist }) => {
