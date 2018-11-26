49c272b278858cb57aab86f98f513cc3e1356d4b
taohua <max.liu@xuantonginfo.com>
2018-11-26 14:45:46 +0800

    generic

--- ts-demo/generics.js
+++ ts-demo/generics.js
@@ -1,7 +1,22 @@
 function identity(arg) {
     return arg;
 }
-function identity2(a, b) {
-    return a + b;
-}
 var myIdentity = identity;
+var GenericNumber = /** @class */ (function () {
+    function GenericNumber() {
+    }
+    return GenericNumber;
+}());
+var myGenericNumber = new GenericNumber();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) {
+    return x + y;
+};
+function loggingIdentity(arg) {
+    console.log(arg.length);
+    return arg;
+}
+loggingIdentity('1231');
+function create(c) {
+    return new c();
+}
--- ts-demo/generics.ts
+++ ts-demo/generics.ts
@@ -1,8 +1,36 @@
-function identity<T> (arg: T): T {
+
+interface GenericIdentityFn<T> {
+  (arg: T): T
+}
+function identity<T>(arg: T): T {
   return arg
 }
-function identity2(a:number, b:string):string{
-  return a+b
+let myIdentity: GenericIdentityFn<number> = identity
+
+class GenericNumber<T> {
+  zeroValue: T;
+  add: (x:T,y:T) => T
+}
+let myGenericNumber = new GenericNumber<number>()
+myGenericNumber.zeroValue = 0
+myGenericNumber.add = function(x,y) {
+  return x+y
 }
 
-let myIdentity: <T> (arg: T) => T = identity
+interface Lengthwise {
+  length: number
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+  console.log(arg.length)
+  return arg
+}
+
+loggingIdentity('1231')
+
+function create<T>(c: {new():T}):T{
+  return new c()
+}
+class BeeKeeper {
+  hasMesk: boolean
+}
