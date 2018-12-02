227bfee3f64eaef4119a6a8643c9b23b96011b35
taohua <max.liu@xuantonginfo.com>
2018-12-02 11:25:09 +0800

    rerestart yarn

--- modules.d.ts
+++ modules.d.ts
@@ -12,4 +12,6 @@ declare module 'react-redux';
 // declare module 'redux-persist';
 declare module 'redux-persist/es/integration/react';
 declare module 'redux-persist/es/storage';
-// declare module 'react-quill';
+declare module 'react-quill';
+// declare module 'deep-equal';
+// declare module 'extend';
