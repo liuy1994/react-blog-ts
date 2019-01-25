2cc494ec573ae8b58e5f7d2979ca2366d249a698
taohua <max.liu@xuantonginfo.com>
2019-01-25 17:06:09 +0800

    fix

--- modules.d.ts
+++ modules.d.ts
@@ -1,13 +1,13 @@
-
 declare module 'md5' {
-  const md5: any;
-  export default md5;
+    const md5: any;
+    export default md5;
 }
 declare module 'axios' {
-  const axios: any;
-  export default axios;
+    const axios: any;
+    export default axios;
 }
-// declare module 'redux';
+
+declare module 'antd';
 declare module 'react-redux';
 // declare module 'redux-persist';
 declare module 'redux-persist/es/integration/react';
