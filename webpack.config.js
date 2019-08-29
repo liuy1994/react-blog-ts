a179ff845a0e62cf8f0ce4b119aa53792bfc31d8
Liu Yong <liuy1994@outlook.com>
2019-08-30 04:14:45 +0800

    'faster'

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const path = require('path')
 const WebpackBundleAnalyzer = require('webpack-bundle-analyzer')
-// const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const extractCSS = new ExtractTextPlugin('stylesheets/[name]-one.css');
+const extractSCSS = new ExtractTextPlugin('stylesheets/[name]-two.css');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CompressionPlugin = require("compression-webpack-plugin")
 let config = {
     entry: {
-        main: './src/index.tsx'
+        main: './src/index.tsx',
+        vendor: ['react', 'react-dom', 'redux', 'react-redux', 'antd'],
     },
     output: {
         filename: '[name].[hash].js',
@@ -19,7 +23,7 @@ let config = {
         port: 9090,
         proxy: {
             // '/': 'http://47.52.234.164:9000'
-            '/': 'http://127.0.0.1:9000'
+            '/blog': 'http://127.0.0.1:9000/'
         }
     },
     devtool: 'inline-source-map',
@@ -37,8 +41,8 @@ let config = {
             //         use: ['css-loader', 'sass-loader']
             //     })
             // },
-            {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
-            {test: /\.css$/, use: ['style-loader', 'css-loader']}
+            {test: /\.scss$/, use: extractSCSS.extract({ fallback: 'style-loader', use: [ 'css-loader', 'sass-loader' ] })},
+            {test: /\.css$/, use: extractCSS.extract({ fallback: 'style-loader', use: [ 'css-loader' ] })}
             // {
             //     test: /\.css$/,
             //     use: ExtractTextPlugin.extract({
@@ -53,9 +57,46 @@ let config = {
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
+        extractCSS,
+        extractSCSS,
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: Infinity,
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            minChunks: function(module) {
+                return (
+                    module.resource &&
+                    /\.js$/.test(module.resource) &&
+                    module.resource.indexOf(
+                        path.join(__dirname, './node_modules')
+                    ) === 0
+                )
+            },
+            chunks: ['index'],
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest',
+            chunks: ['vendor', 'common', 'index']
+        }),
         new WebpackBundleAnalyzer.BundleAnalyzerPlugin(),
-        // new ExtractTextPlugin('styles.css'),
-        new UglifyJsPlugin(),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                ie8: false,
+                mangle: true,
+                output: { comments: false },
+                compress: {
+                    warnings: false,
+                    drop_console: true,
+                    drop_debugger: true,
+                    unused: false,
+                },
+            },
+            sourceMap: true,
+            cache: true,
+        }),
+        new CompressionPlugin(),
         new webpack.HotModuleReplacementPlugin()
     ]
 }
